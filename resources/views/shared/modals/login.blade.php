4324217d948940305c3067f67d0af098448865ff
Michal Gasparik <mgasparik@ssp-petermezes-ste22.local>
2017-02-18 22:28:30 +0100

    sign up text in modal

--- resources/views/shared/modals/login.blade.php
+++ resources/views/shared/modals/login.blade.php
@@ -1,23 +1,38 @@
 <!-- Modal -->
 <div aria-labelledby="myModalLabel" class="modal fade" id="myModal" role="dialog" tabindex="-1">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <button aria-label="Close" class="close" data-dismiss="modal" type="button"><span aria-hidden="true">&times;</span></button>
                 <h4 class="modal-title" id="myModalLabel">Sign In</h4>
             </div>
             <div class="modal-body">
-                <form class="ajax" action="/signup" method="post" role="form">
-                    <div class="form-group">
-                        <label for="email">Email address:</label> <input class="form-control" id="email" type="email">
-                    </div>
-                    <div class="form-group">
-                        <label for="pwd">Password:</label> <input class="form-control" id="pwd" type="password">
-                    </div>
-                    <div class="checkbox">
-                        <label><input type="checkbox"> Remember me</label>
-                    </div><button class="btn btn-primary" type="submit">Sign In</button>
-                </form>
+                <div class="col-md-7">
+                    <section class="sign-in">
+                        <form class="ajax" action="/signup" method="post" role="form">
+                            <div class="form-group">
+                                <label for="email">Email address:</label> <input class="form-control" id="email" type="email">
+                            </div>
+                            <div class="form-group">
+                                <label for="pwd">Password:</label> <input class="form-control" id="pwd" type="password">
+                            </div>
+                            <div class="checkbox">
+                                <label><input type="checkbox"> Remember me</label>
+                            </div><button class="btn btn-primary btn-lg" type="submit">Sign In</button>
+                        </form>
+                    </section>
+                </div>
+
+                <div class="col-md-5">
+                    <section class="sign-up">
+                        <h3>You Don't have account?</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+                            laoreet dolore magna aliquam erat volutpat <a href="#">Sign up</a>.
+                        </p>
+                    </section>
+                </div>
+                <div class="clearfix"></div>
             </div>
         </div>
     </div>
