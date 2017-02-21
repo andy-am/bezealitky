a20d6223f3ca055e29355cc1ed9ce18c48b41a08
Michal Gasparik <mgasparik@ssp-petermezes-ste22.local>
2017-02-21 14:10:57 +0100

    homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,9 @@
-@extends('layouts.app')
+@extends('layouts.master')
+@section('bodyClass', 'homepage')
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+@section('main')
+    <div class="center-box">
+        <h1><span>Flatter</span><br> is watching you!</h1>
+        <p>Welcome to closed alpha :)</p>
     </div>
-</div>
 @endsection
